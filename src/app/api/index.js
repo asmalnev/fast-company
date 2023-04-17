e4239f290660001f0cb279249331c6248571d12a
Aleksandr Malnev <asmalnev@gmail.com>
2023-04-17 16:26:55 +0300

    проект приведен к исходному состоянию

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -3,5 +3,4 @@ import * as users from './fake.api/user.api';
 const API = {
   users
 };
-
 export default API;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
-import Users from './components/Users';
+import Users from './app/components/users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
