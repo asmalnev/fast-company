eff1b2631679484038f28ad87bf208c2876f513e
Aleksandr Malnev <asmalnev@gmail.com>
2023-06-12 22:33:47 +0300

    checkbox error

--- src/components/common/form/CheckBoxField.jsx
+++ src/components/common/form/CheckBoxField.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const CheckBoxField = ({ name, onChange, value, children }) => {
+const CheckBoxField = ({ name, onChange, value, children, error }) => {
   const handleChange = () => {
     onChange({ name, value: !value });
+  };
+
+  const getInputClasses = () => {
+    return "form-check-input" + (error ? " is-invalid" : "");
   };
 
   return (
     <div className="form-check mb-4">
       <input
-        className="form-check-input"
+        className={getInputClasses()}
         type="checkbox"
         id={name}
         value=""
@@ -19,6 +23,7 @@ const CheckBoxField = ({ name, onChange, value, children }) => {
       <label className="form-check-label" htmlFor={name}>
         {children}
       </label>
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
@@ -30,7 +35,8 @@ CheckBoxField.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
-  ])
+  ]),
+  error: PropTypes.string
 };
 
 export default CheckBoxField;
